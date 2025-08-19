f0706151ebba165af905585114ef3ff17ff5be89
Himanshu <himbharti9902@gmail.com>
2025-08-20 00:20:53 +0530

    debug 5

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,9 +32,9 @@ app.use(fileUpload({
 }));
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'../frontend','dist')));
+    app.use(express.static(path.join(__dirname,'../../frontend','dist')));
     app.get('/*catchAll',(req,res)=>{
-        res.sendFile(path.join(__dirname,'../frontend','dist','index.html'));
+        res.sendFile(path.join(__dirname,'../../frontend','dist','index.html'));
     })
 }
 app.use('/api/user',userRouter);
